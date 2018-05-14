a284cf3c5be75c141549f5c4e60520b6fb9eb664
JonyYu <328553173@qq.com>
2018-05-14 12:04:14 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import './common/css/common.css';
 import './components/layer/layer.less';
 import Layer from './components/layer/layer.js';
-import {React} from 'react';
+import React from 'react';
 class Greeting extends React.component{
   render(){
     return <h1>Hello world</h1>;
